2dbda99973756c140bb8d9a85f18e80cb661ff97
adrianpusty <adrianpu92@wp.pl>
2021-04-29 21:57:58 +0200

    io and nio charts added

--- cheatsheet/shell.sh
+++ cheatsheet/shell.sh
@@ -1,8 +1,10 @@
 mkdir -p 	# No error if existing, make parent directories as needed
-top -c | grep 12345 # Provide PID instead of 12345
+top -c | grep 12345 # PID instead of 12345
 
 ls -1 .		# Listing files in a single column
 ls -lh    # -h, --human-readable # print sizes like 1K 234M 2G etc.
+
+cat file1 file2 file3 >> output # merging file1, file2 and file3 to output file
 
 #for loop
 for x in a b c d e f; do
