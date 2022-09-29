528fe1fa647292dddc1eb747d4a926655411e393
Adrian Pusty <51136868+adrianpusty@users.noreply.github.com>
2022-09-29 16:07:14 +0200

    pandas examples: reduce by month, index to datetime, read csv without header and with specified column as index

--- cheatsheet/python/pandas.py
+++ cheatsheet/python/pandas.py
@@ -15,8 +15,12 @@ def create_df():
     df2 = pd.DataFrame(np.array([['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']]),
                        columns=['col_1', 'col_2', 'col_3'])
 
-df = pd.read_csv('in.csv', error_bad_lines=False, usecols=['foo', 'bar'], nrows=10) # nrows (optional parameter) - Number of rows of file to read. Useful for reading pieces of large files.
+df = pd.read_csv('in.csv', error_bad_lines=False, usecols=['foo', 'bar'], nrows=10, header=None, index_col=0) # nrows (optional parameter) - Number of rows of file to read. Useful for reading pieces of large files.
 df.to_csv('out.txt', index=False, header=False, quoting=csv.QUOTE_NONE)
+
+df.index = pd.to_datetime(df.index)
+
+df = df.resample('1M').sum()
 
 df.columns = ['new_col_name_1', 'new_col_name_2'] # renaming column names
 df.head(1) # Return the first n rows. # df.head() - Viewing the first 5 lines # ~https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.head.html
