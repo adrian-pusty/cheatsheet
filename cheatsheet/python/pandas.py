641c6afbe8247b4dad6244efba40ea1b504807ed
Adrian Pusty <51136868+adrianpusty@users.noreply.github.com>
2022-09-29 16:59:14 +0200

    pandas DataFrame sum columns example

--- cheatsheet/python/pandas.py
+++ cheatsheet/python/pandas.py
@@ -18,11 +18,11 @@ def create_df():
 df = pd.read_csv('in.csv', error_bad_lines=False, usecols=['foo', 'bar'], nrows=10, header=None, index_col=0) # nrows (optional parameter) - Number of rows of file to read. Useful for reading pieces of large files.
 df.to_csv('out.txt', index=False, header=False, quoting=csv.QUOTE_NONE)
 
-df.index = pd.to_datetime(df.index)
-
 df = df.resample('1M').sum()
 
+df.index = pd.to_datetime(df.index)
 df.columns = ['new_col_name_1', 'new_col_name_2'] # renaming column names
+
 df.head(1) # Return the first n rows. # df.head() - Viewing the first 5 lines # ~https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.head.html
 
 df['col1'] = df['col1'].str.replace(',', '-')
@@ -33,6 +33,9 @@ df['col1'] = 'my_prefix' + df['col1'].astype(str)
 df = df[(df['col1'] == 'val1') & (df['col2'] == 123)]
 df = df[df['col1'] != 123]
 df = df[df.col1 > 100] # only rows where col greater than 100
+
+df['col7'] = df.iloc[:, 0: 6].sum(axis=1)       # sum columns (from 0th to 6th column)
+df['col8'] = df[['col1', 'col4']].sum(axis=1)   # sum columns ('col1' and 'col4')
 
 df = df.drop('1', axis=0)       # delete row '1'
 df = df.drop('col_1', axis=1)   # delete column 'col_1'
