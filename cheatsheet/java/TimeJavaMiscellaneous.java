587c2e6635acf3f5241e85c20925122f7463eb0d
Adrian Pusty <51136868+adrianpusty@users.noreply.github.com>
2022-10-17 10:50:20 +0200

    Create TimeJavaMiscellaneous.java

---
+++ cheatsheet/java/TimeJavaMiscellaneous.java
@@ -0,0 +1,33 @@
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+class TimeJavaMiscellaneous
+{
+    public long daysDiff() throws ParseException
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date firstDate = sdf.parse("1992-05-04");
+        Date secondDate = sdf.parse("1992-05-05");
+
+        return TimeUnit.DAYS.convert((secondDate.getTime() - firstDate.getTime()), TimeUnit.MILLISECONDS);
+    }
+
+    public void createDateAndTimestampWithCurrentTime()
+    {
+        new Timestamp(System.currentTimeMillis());
+        new Timestamp(new Date().getTime());
+
+        new Date();
+    }
+
+    public LocalTime parseExample(String val)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm:ss");
+        return LocalTime.parse(val, formatter);
+    }
+}
