e49f0bf604d80d1b53ac5b45b4e5049647ded084
Adrian Pusty <51136868+adrianpusty@users.noreply.github.com>
2023-03-17 12:14:00 +0100

    if / elif in .sh cheatsheet

--- command_line/scripts.sh
+++ command_line/scripts.sh
@@ -21,6 +21,23 @@ for ((i=1;i<=END;i++)); do
     fi
 done
 
-read -r number1
-read -p "Please enter a number2: " number2
-echo $((${number1}+${number2}))
+read -r val1
+read -p "Please enter second value: " val2
+echo $((${val1}+${val2}))
+
+if [ ${val1} -gt ${val2} ]; then
+	echo "val1 is greater than val2";
+fi
+
+if [ "${val1}" == 'Y' ] || [ ${val1} == 3 ] # note the spaces after '[' and before ']'
+then 
+	echo "val1 is equal to 'Y' or 3";
+elif ((${val1} < ${val2})) # won't work with -lt
+then 
+	echo "val1 is less than val2 and not equal to 'Y' or 3";
+fi
+
+# if ((${val1} -lt ${val2})) # won't work with -lt # error
+# then
+# 	echo "val1 is less than val2";
+# fi
